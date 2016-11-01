9a1e42a9f7dd9ae736d0ab945a5bad363ffef929
Ján Kobezda <jan@kobezda.net>
2016-11-01 19:12:02 +0100

    WIP: Add Nginx generator

--- brutus/generate.py
+++ brutus/generate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import jinja2
 
 rootdir = "output"
 
@@ -31,7 +32,26 @@ class DovecotGenerate:
                 if {"domain", "password"}.issubset(item.keys()):
                     print("{id}@{domain}:{{{password[scheme]}}}{password[data]}".format(**item), file=stream)
 
+class NginxGenerate:
+    def __init__(self, db):
+        self.db = db
+        self.basedir =  os.path.join(rootdir, "nginx")
+
+    def generate(self):
+
+        templateLoader = jinja2.FileSystemLoader(searchpath="templates/")
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        nginx_site_template_name = "nginx/site.conf"
+        template = templateEnv.get_template(nginx_site_template_name)
+
+        for item in self.db["websites"].values():
+            filename = os.path.join(self.basedir, item['id'] + ".conf")
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            with open(filename, "w") as stream:
+                output = template.render(item)
+                print(output, file=stream)
 
 def generate_all(db):
     PostfixGenerate(db).generate()
     DovecotGenerate(db).generate()
+    NginxGenerate(db).generate()
