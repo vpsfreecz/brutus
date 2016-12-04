5cf7083adc0c1bb2190efab96835fae126b044da
Ján Kobezda <jan@kobezda.net>
2016-12-04 18:49:43 +0100

    Let's encrypt

--- brutus/generate.py
+++ brutus/generate.py
@@ -8,6 +8,9 @@ from io import open
 import os
 import jinja2
 import yaml
+import base64
+import subprocess
+import tempfile
 
 from . import utils
 
@@ -22,9 +25,10 @@ def register(cls):
 class Generate:
     template_env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath="templates"), trim_blocks=True)
 
-    def __init__(self, db, rootdir):
+    def __init__(self, db, rootdir, generate_keys):
         self.db = db
         self.rootdir = rootdir
+        self.generate_keys = generate_keys
 
     def generate_file(self, name, **kargs):
         template = "{}/{}.j2".format(self.name, name)
@@ -171,8 +175,19 @@ class KnotGenerate(Generate):
         basedir = os.path.join(self.rootdir, "knot", "etc", "knot")
 
         template = self.template_env.get_template('knot/knot.conf.j2')
+
+        # for LE
+        for domain in self.db['domains']:
+            if 'tsigid' not in self.db["domains"][domain]:
+                self.db["domains"][domain]["tsigid"] = domain + ".LE.KEY"
+                self.db["domains"][domain]["aclid"] = domain + ".LE.ACL"
+                randomtext = os.urandom(int(int(256)/8))
+                tsigsecret = base64.b64encode(randomtext).decode("utf-8")
+                self.db["domains"][domain]["tsigsecret"] = tsigsecret if self.generate_keys else ""
+
         variables = {}
         variables['domains'] = self.db["domains"]
+
         filename = os.path.join(basedir, "knot.conf")
         utils.makedirs(os.path.dirname(filename))
         with open(filename, "w") as stream:
@@ -180,10 +195,69 @@ class KnotGenerate(Generate):
             print(output, file=stream)
 
 
-def generate_all(db, rootdir):
+@register
+class LetsencryptGenerate(Generate):
+    service = "certificates"
+    name = "letsencrypt"
+
+    def create_RSA_key(self):
+        return subprocess.check_output(["openssl", "genrsa", "4096"])
+
+    def create_CSR(self, san, key):
+        with open("/etc/ssl/openssl.cnf") as sslconf:
+            openssl_csr_config = sslconf.read()
+        san_config = "[SAN]\nsubjectAltName=" + ",".join(["DNS:" + domain for domain in san])
+        openssl_csr_config = openssl_csr_config + san_config
+
+        tempdir = tempfile.mkdtemp()
+        keyfifo = os.path.join(tempdir, 'keyfifo')
+        conffifo = os.path.join(tempdir, 'conffifo')
+        keyfd = os.open(keyfifo, os.O_WRONLY|os.O_CREAT)
+        os.write(keyfd, key)
+        os.close(keyfd)
+        with open(conffifo, 'w') as pipe:
+            pipe.write(openssl_csr_config)
+        ossl = subprocess.Popen(['openssl', 'req', '-new', '-sha256', '-key', keyfifo, '-subj', '/', '-reqexts', 'SAN', '-config', conffifo], stdout=subprocess.PIPE)
+        out, err = ossl.communicate()
+        return out
+
+    def generate(self):
+        if self.generate_keys and 'letsencrypt_account_key' not in self.db["instances"][None]:
+            self.db["instances"][None]["letsencrypt_account_key"] = self.create_RSA_key()
+
+        basedir = os.path.join(self.rootdir, self.name, "etc", "letsencrypt")
+        template = self.template_env.get_template('letsencrypt/le.ini.j2')
+        for name, value in sorted(self.db["domains"].items()):
+            if ('dns' not in value['services']) or ('tsigsecret' not in value):
+                continue
+            variables = {
+                'domain': name,
+                'tsigid': value['tsigid'],
+                'tsigsecret': value['tsigsecret'] if self.generate_keys else "",
+                'admin_mail': self.db["instances"][None].get("admin_mail", ""),
+                'acme_dir': 'https://acme-staging.api.letsencrypt.org/directory',
+                'dns': 'localhost',
+            }
+            if self.generate_keys and 'letsencrypt_domain_key' not in value:
+                self.db["domains"][name]["letsencrypt_domain_key"] = self.create_RSA_key()
+                san = [domain for domain in self.db["websites"]]
+                csr = self.create_CSR(san, self.db["domains"][name]["letsencrypt_domain_key"])
+                utils.makedirs(basedir)
+                keyfd = os.open(os.path.join(basedir, name + '.csr'), os.O_WRONLY | os.O_CREAT)
+                os.write(keyfd, csr)
+                os.close(keyfd)
+
+            filename = os.path.join(basedir, name + ".ini")
+            utils.makedirs(os.path.dirname(filename))
+            with open(filename, "w") as stream:
+                output = template.render(variables)
+                print(output, file=stream)
+
+
+def generate_all(db, rootdir, generate_keys=True):
     for cls in registered_classes:
         services = db["instances"][None]["services"]
         # Skip disabled services
         if not services.get(cls.service):
             continue
-        cls(db, rootdir).generate()
+        cls(db, rootdir, generate_keys).generate()
