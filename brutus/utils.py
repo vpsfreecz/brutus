cf87536939c0183c9c8f568ee88a3fd79f01ef08
Pavel Šimerda <pavlix@pavlix.net>
2016-12-04 18:46:08 +0100

    utils: add symlink function

--- brutus/utils.py
+++ brutus/utils.py
@@ -3,11 +3,14 @@
 """ Miscellaneous helper functions """
 import os
 
+def run_without_exception(function, exc_type, condition=lambda e: True):
+    def wrapper(*args, **kargs):
+        try:
+            function(*args, **kargs)
+        except exc_type as e:
+            if not condition(e):
+                raise
+    return wrapper
 
-def makedirs(path):
-    """Emulates os.makedirs(exist_ok=True)"""
-    try:
-        os.makedirs(path)
-    except OSError as e:
-        if e.errno != 17:
-            raise
+makedirs = run_without_exception(os.makedirs, OSError, lambda e: e.errno == 17)
+symlink = run_without_exception(os.symlink, OSError, lambda e: e.errno == 17)
